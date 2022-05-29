51ebbd361c174e7a55f97f0c7f76d49520f8479d
ViniciusFelce01~ <viramos017@gmail.com>
2022-05-29 02:53:38 -0300

    vini atualizou!

--- js/validacaoForms.js
+++ js/validacaoForms.js
@@ -1,28 +1,96 @@
 const gameNameForm = document.getElementById('gameNameForm');
 const gameReleaseDateForm = document.getElementById('gameReleaseDateForm');
-const formulario = document.getElementById('formulario');
 const gameDescriptionForm = document.getElementById('gameDescriptionForm');
 const gameIMGForm = document.getElementById('gameIMGForm');
-const error = document.querySelectorAll('.error');
-
-formulario.addEventListener('submit', (e) =>{
-
+const placeholder = document.querySelectorAll('.placeholder');
+const ic1 = document.querySelectorAll('.ic1');
+const form = document.getElementById('form');
+//----------submit-----------------
+form.addEventListener('submit', (e) =>{
    checkName();
-
+   CheckReleaseDate();
+   CheckDescription();
+   CheckIMG();
+//-----------gameNameForm----------------
 function checkName(){
-   
+    let mensplaceholder =[];
     if(gameNameForm.value === '' || gameNameForm.value == null){
-        e.preventDefault();
-        
-        formulario[0].classList.add('fail')
-    } 
+        e.preventDefault(); 
+        mensplaceholder.push('Adicione um Nome!!!')
+        placeholder[0].innerText = mensplaceholder;
+        ic1[0].classList.add('fail');
+    }
     else{
-        formItem[0].classList.remove('fail');
-        formItem[0].classList.add('sucess');
+        ic1[0].classList.remove('fail');
+        placeholder[0].innerText = null;
+        ic1[0].classList.add('sucess');
+        mensplaceholder.push('Nome do Jogo')
+        placeholder[0].innerText = mensplaceholder;
     }
 }
+//-----------gameReleaseDateForm----------------
+function CheckReleaseDate(){
+    let mensplaceholder = [];
+    if(gameReleaseDateForm.value.length == 0 ){
+        e.preventDefault();
+        mensplaceholder.push('Adicione uma data!!!')
+        placeholder[1].innerText = mensplaceholder;
+        ic1[1].classList.add('fail');
+    }
+    else if (gameReleaseDateForm.value >= 2023 ){
+        e.preventDefault();
+        mensplaceholder.push('Pega leve ai John Connor!!!')
+        placeholder[1].innerText = mensplaceholder;
+        ic1[1].classList.add('fail');
+    }
+    else if (gameReleaseDateForm.value <= 1980 ){
+        e.preventDefault();
+        mensplaceholder.push('Sem viagens No tempo!!!')
+        placeholder[1].innerText = mensplaceholder;
+        ic1[1].classList.add('fail');
+    }
+    else {
+        ic1[1].classList.remove('fail');
+        placeholder[1].innerText = null;
+        ic1[1].classList.add('sucess');
+        mensplaceholder.push('Ano de Lançamento')
+        placeholder[1].innerText = mensplaceholder;
+    }
 
-
+}
+//-----------gameDescriptionForm----------------B.O.zinho não resulvido por(vini) = Help!
+function CheckDescription(){
+    //  let mensplaceholder = []
+    //  if(gameDescriptionForm.value === '' || gameDescriptionForm.value == null){
+    //      e.preventDefault();
+    //      mensplaceholder.push('Adicione uma descrição test1');
+    //      placeholder[2].innerText = mensplaceholder;
+    //      ic1[2].classList.add('fail');
+    //  }
+    //  else {
+    //      ic1[2].classList.remove('fail');
+    //      placeholder[2].innerText = null;
+    //      ic1[2].classList.add('sucess');
+    //      mensplaceholder.push('Descrição do Jogo test2')
+    //      placeholder[2].innerText = mensplaceholder;
+    //  }
+}
+//------------gameIMGForm---------------B.O.zinho2 não resulvido por(vini) = Help!
+function CheckIMG(){
+    //  let mensplaceholder = []
+    //  if (gameIMGForm.nodeValue === '' || gameIMGForm == null) {
+    //      e.preventDefault();
+    //      mensplaceholder.push('Adinione uma IMG com URL 111');
+    //      placeholder[2].innerText= mensplaceholder;
+    //      ic1[2].classList.add('fail');
+    //  }
+    //  else {
+    //      ic1[2].classList.remove('fail');
+    //      placeholder[2].innerText = null;
+    //      ic1[2].classList.add('sucess');
+    //      mensplaceholder.push('Link da Capa do Jogo 222')
+    //      placeholder[2].innerText = mensplaceholder;
+    //  }
+}
+//---------------------------
 })
-
-
